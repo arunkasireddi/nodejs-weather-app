91bc396b403d9ebde92a7de2562fb32d6041b9e2
arunkasireddi <arun.kasireddi@gmail.com>
2017-09-09 19:09:30 +0100

    Completed promises get weather functionality

--- app-promise.js
+++ app-promise.js
@@ -10,15 +10,31 @@ const argv = yargs
             string: true
         }
     })
-    .help()
-    .alias('help', 'h')
+    .help().alias('help', 'h')
     .argv;
 
 var encodedAddress = encodeURIComponent(argv.address);
 var geocodeUrl = `https://maps.googleapis.com/maps/api/geocode/json?address=${encodedAddress}`;
-
 axios.get(geocodeUrl).then((response) => {
-    console.log(response.data);
+    if (response.data.status === 'ZERO_RESULTS') {
+        throw new Error('Unable to find that address');
+    }
+    var lat = response.data.results[0].geometry.location.lat;
+    var lon = response.data.results[0].geometry.location.lng;
+
+    var weatherUrl = `https://api.darksky.net/forecast/51db297c0dc81dcb16277b12670c05a9/${lat},${lon}`;
+    console.log(JSON.stringify(response.data.results[0].formatted_address, undefined, 2));
+
+    return axios.get(weatherUrl);
+
+}).then((response) => {
+    var temperature = response.data.currently.temperature;
+    var apparentTemperature = response.data.currently.apparentTemperature;
+    console.log(`It's currently ${temperature}. It feels like ${apparentTemperature}.`);
 }).catch((e) => {
-    console.log(e);
+    if (e.code === 'ENOTFOUND') {
+        console.log('Unable to connect to API servers');
+    } else {
+        console.log(e.message);
+    }
 });
